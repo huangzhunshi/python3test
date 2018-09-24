22ba3d5d3fa39f25a59d57d7b850046e7632cf10
yihui <123>
2018-09-24 18:29:58 +0800

    提交新的方法，sklearn的教程

---
+++ sk/demo01.py
@@ -0,0 +1,10 @@
+'''
+https://blog.csdn.net/qq_16234613/article/details/76534673 机器学习url
+
+https://blog.csdn.net/kevinelstri/article/details/60960574 机器学习教程
+'''
+
+from sklearn import datasets
+iris = datasets.load_iris()
+
+print(iris)
