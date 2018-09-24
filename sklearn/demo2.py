79db6cca3be0b954622645e46a1cd7c7f0712b0d
yihui <123>
2018-09-24 15:52:43 +0800

    提交新方法

---
+++ sklearn/demo2.py
@@ -0,0 +1,16 @@
+'''
+机器学习相关地址
+https://www.cnblogs.com/lianyingteng/p/7749609.html
+'''
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+model = LinearRegression()
+
+#print(model)
+
+#sklearn.model_selection(X, y, test_size=0.33, random_state=42)
+
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+#
+# print(X_test)
